7cfdaa905d2f22f13e8d6d627d7dc3245a80cb8b
Evelyn_ Hernandez <eh22@usf.edu>
2024-10-06 22:18:58 -0400

    Create Department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -10,3 +10,16 @@ class Employee{
         return `name${this.name}, position${this.position},salary${this.salary}`
     }
 }
+// Task 2: Create a Department Class
+class Department {
+    constructor(name){
+        this.name = name;
+        this.employees = [];
+    }
+    addEmployee(employee){
+        this.employees.push(employee);
+    }
+    getDepartmentSalary(){
+        return this.employees.reduce((total,manager) => total + manager.salary + manager, 0)
+    }
+}
