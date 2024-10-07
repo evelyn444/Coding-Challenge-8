4ac530b3b298a84f2fc60f67d370a21aed0a8efa
Evelyn_ Hernandez <eh22@usf.edu>
2024-10-06 22:47:08 -0400

    Fixing error in task 4

--- employee_management.js
+++ employee_management.js
@@ -10,19 +10,6 @@ class Employee{
         return `name${this.name}, position${this.position},salary${this.salary}`
     }
 }
-// Task 2: Create a Department Class
-class Department {
-    constructor(name){
-        this.name = name;
-        this.employees = [];
-    }
-    addEmployee(employee){
-        this.employees.push(employee);
-    }
-    getDepartmentSalary(){
-        return this.employees.reduce((total,employee) => total + employee.salary, 0)
-    }
-}
 //Task 3: Create a Manager Class that Inherits from Employee
 class Manager extends Employee {
     constructor(name,salary,position,department,bonus){
@@ -33,11 +20,31 @@ class Manager extends Employee {
         return `name${this.name}, position${this.position}, salary${this.salary}, bonus${this.bonus}`
     }
 }
+// Task 2: Create a Department Class
+class Department {
+    constructor(name){
+        this.name = name;
+        this.employees = [];
+    }
+    addEmployee(employee){
+        this.employees.push(employee);
+    }
+    getDepartmentSalary(){
+        return this.employees.reduce((total,employee) => total + employee.salary, 0);
+    }
+
 
 //Task 4: Handle Bonuses for Managers
-calculateTotalSalaryWithBonus() {
-    return this.employees.reduce((total, manager)=> total + manager.salary + manager, 0)
+calculateTotalSalaryWithBonus(){
+    return this.employees.reduce ((total, employee) => {
+        if (employee in Manager) {
+            return total + employee.salary + employee.bonus;
+        }
+    }, 0 );
 }
+}
+
+
 //Task 5: Create and Manage Departments and Employees
 //Departments
 const engineering = new Department("Engineering");
